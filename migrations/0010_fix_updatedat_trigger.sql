f124cadc8c17910549aa865887c110d60d24f5e7
Mike M <watkajtys@gmail.com>
2025-07-09 16:20:48 -0600

    migrations needed on production

--- migrations/0010_fix_updatedat_trigger.sql
+++ migrations/0010_fix_updatedat_trigger.sql
@@ -1,7 +1,3 @@
--- Migration to add updatedAt trigger to ClusterDefinitions table
-
--- Optional: Trigger to automatically update `updatedAt` timestamp on row modification.
--- This might need to be adapted based on D1's exact SQL dialect for triggers.
 DROP TRIGGER IF EXISTS trg_ClusterDefinitions_AutoUpdate_UpdatedAt;
 CREATE TRIGGER trg_ClusterDefinitions_AutoUpdate_UpdatedAt
 AFTER UPDATE ON ClusterDefinitions
